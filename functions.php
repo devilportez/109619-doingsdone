673d8358777b03d3589a7026dd3fd50f55ec9198
Stas Melekhov (devilportez) <devilportez@icloud.com>
2018-02-11 01:24:05 +0500

    Changes via comments V2

--- functions.php
+++ functions.php
@@ -8,4 +8,50 @@ function set_template ($template, $data) {
     }
     return "";
 }
+
+function get_tasks_amount ($tasks, $project) {
+    $count = 0;
+    foreach ($tasks as $task) {
+        if ($project === "Все") {
+            $count = count($tasks);
+        }
+        if ($task["category"] === $project) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+function get_urgent_task ($date) {
+    $current_timestamp = time();
+    $task_timestamp = strtotime($date);
+    $seconds_in_day = 86400;
+    $difference = floor(($task_timestamp - $current_timestamp) / $seconds_in_day);
+    if ($difference < 1) {
+        return true;
+    }
+    return false;
+}
+
+function filter_tasks ($tasks, $project, $show_complete_tasks) {
+    $filtered_tasks = [];
+    if ($project === "Все") {
+        $filtered_tasks = $tasks;
+    }
+    foreach ($tasks as $key => $task) {
+        if ($show_complete_tasks) {
+            if ($project === $task["category"]) {
+                $filtered_tasks[] = $tasks[$key];
+            }
+        } else {
+            if ($project === "Все" && !$task["is_completed"]) {
+                $filtered_tasks[] = $tasks[$key];
+            }
+            if ($project === $task["category"] && !$task["is_completed"]) {
+                $filtered_tasks[] = $tasks[$key];
+            }
+        }
+    }
+    return $filtered_tasks;
+}
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 require_once("functions.php");
 
+$PROJECT_ALL_TASKS = 0;
+
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 
@@ -52,66 +54,19 @@ $tasks = [
     ]
 ];
 
-function get_tasks_amount ($tasks, $project) {
-    $count = 0;
-    foreach ($tasks as $task) {
-        if ($project === "Все") {
-            $count = count($tasks);
-        }
-        if ($task["category"] === $project) {
-            $count++;
-        }
-    }
-    return $count;
-}
-
-function get_urgent_task ($date) {
-    $current_timestamp = time();
-    $task_timestamp = strtotime($date);
-    $seconds_in_day = 86400;
-    $difference = floor(($task_timestamp - $current_timestamp) / $seconds_in_day);
-    if ($difference < 1) {
-        return true;
-    }
-    return false;
-}
-
-function filter_tasks ($tasks, $show_complete_tasks, $project = "Все") {
-    $filtered_tasks = [];
-    foreach ($tasks as $key => $task) {
-        if ($show_complete_tasks) {
-            if ($project === "Все") {
-                $filtered_tasks = $tasks;
-            }
-            if ($project === $task["category"]) {
-                $filtered_tasks[] = $tasks[$key];
-            }
-        } else {
-            if ($project === "Все" && !$task["is_completed"]) {
-                $filtered_tasks[] = $tasks[$key];
-            }
-            if ($project === $task["category"] && !$task["is_completed"]) {
-                $filtered_tasks[] = $tasks[$key];
-            }
-        }
-    }
-    return $filtered_tasks;
-}
-
 if (isset($_GET["project_id"])) {
-    $PROJECT_ALL_TASKS = 0;
     $project_id = (int) $_GET["project_id"];
     $project_tasks = [];
     if ($project_id === $PROJECT_ALL_TASKS) {
-        $project_tasks = filter_tasks($tasks, $show_complete_tasks);
+        $project_tasks = filter_tasks($tasks, $projects[$PROJECT_ALL_TASKS], $show_complete_tasks);
     } elseif (isset($projects[$project_id])) {
-        $project_tasks = filter_tasks($tasks, $show_complete_tasks, $projects[$project_id]);
+        $project_tasks = filter_tasks($tasks, $projects[$project_id], $show_complete_tasks);
     } else {
         http_response_code(404);
         $message = "Проектов с таким id не найдено.";
     }
 } else {
-    $project_tasks = filter_tasks($tasks, $show_complete_tasks);
+    $project_tasks = filter_tasks($tasks, $projects[$PROJECT_ALL_TASKS], $show_complete_tasks);
 }
 
 if (http_response_code() === 404) {
