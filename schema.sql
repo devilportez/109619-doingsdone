d23007897a2592b97d4c50b3f720d4f7fb881d7c
Stas Melekhov (devilportez) <devilportez@icloud.com>
2018-02-21 12:02:02 +0500

    Edit schema

--- schema.sql
+++ schema.sql
@@ -1,27 +1,51 @@
-CREATE DATABASE doingsdone;
-DEFAULT CHARACTER SET utf8;
-DEFAULT COLLATE utf8_general_ci;
+CREATE DATABASE doingsdone
+  DEFAULT CHARACTER SET utf8
+  DEFAULT COLLATE utf8_general_ci;
+
 USE doingsdone;
 
-CREATE TABLE users (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  signup_date DATETIME,
-  email CHAR,
-  name CHAR,
-  password CHAR,
-  contacts TEXT
+CREATE TABLE projects (
+	id int NOT NULL AUTO_INCREMENT,
+	name char NOT NULL UNIQUE,
+	user_id int NOT NULL,
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE projects (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name CHAR
+CREATE TABLE users (
+	id int NOT NULL AUTO_INCREMENT,
+	signup_date DATETIME NOT NULL,
+	email char NOT NULL UNIQUE,
+	name char NOT NULL,
+	password char NOT NULL,
+	contacts char,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE tasks (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  name CHAR,
-  create_date DATETIME,
-  done_date DATETIME,
-  file CHAR,
-  term DATETIME
+	id int NOT NULL AUTO_INCREMENT,
+	create_date DATETIME,
+	done_date DATETIME,
+	name char NOT NULL,
+	file char,
+	deadline DATETIME,
+	user_id int NOT NULL,
+	project_id int NOT NULL,
+	PRIMARY KEY (id)
 );
+
+ALTER TABLE projects
+  ADD CONSTRAINT projects_fk0
+  FOREIGN KEY (user_id)
+  REFERENCES users(id);
+
+ALTER TABLE tasks
+  ADD CONSTRAINT tasks_fk0
+  FOREIGN KEY (user_id)
+  REFERENCES users(id);
+
+ALTER TABLE tasks
+  ADD CONSTRAINT tasks_fk1
+  FOREIGN KEY (project_id)
+  REFERENCES projects(id);
+
+CREATE INDEX deadline ON tasks(deadline)
