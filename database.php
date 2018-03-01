979b2635300bdc6609ed3906fd20b51a42363028
Stas Melekhov (devilportez) <devilportez@icloud.com>
2018-03-02 01:57:45 +0500

    Sey charser

--- database.php
+++ database.php
@@ -9,6 +9,7 @@ $connection = mysqli_connect(
     $dbconn["password"],
     $dbconn["name"]
 );
+mysqli_set_charset($connection, "utf8");
 if (!$connection) {
     print(mysqli_connect_error());
     exit;
